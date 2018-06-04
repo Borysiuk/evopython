d6fe8ed65e2d04c1cca5ad8550378e8021bebaf9
iurii_borysiuk <iurii.borysiuk@gmail.com>
2018-06-05 02:44:02 +0300

    fix break

--- l2.2.py
+++ l2.2.py
@@ -1,11 +1,16 @@
-key = int(input("Сохраните пароль 4 цифры"))
-krf = int(input("Введите пароль"))
+key_save = int(input("Сохраните пароль, введите 4 цифры и нажмите Enter: "))
+key_enter = int(input("Введите пароль: "))
 n = 5
-if krf == key:
+if key_enter == key_save:
     print("Доступ открыт")
 else:
     while n != 0:
         print("Количество попыток", n)
-        krf = int(input("Введите пароль"))
-        n -= 1
-    print("Ваши попытке исчерпаны")
+        key_enter = int(input("Введите пароль: "))
+        if key_enter == key_save:
+            print("Доступ открыт")
+            break
+        else:
+            n -= 1
+    else:
+        print("Ваши попытки исчерпаны")
